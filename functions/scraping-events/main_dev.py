6e1f7cf6d8a39b89ee4404a58a4084e7357a5bf6
Ryan Echols <rkechols21@gmail.com>
2025-06-07 00:15:16 -0600

    rename main to main_cli

--- functions/scraping-events/main_dev.py
+++ functions/scraping-events/main_dev.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
     # load env vars before even importing anything from `scraping_events`
     dotenv.load_dotenv()
 
-    from scraping_events.main import main
+    from scraping_events.main_cli import main
 
     main()
